8ea1c5b9a08fb85917f21cd3b3a26b359110872c
jiaoshibo <jiaoshibo@outlook.com>
2017-10-14 18:41:55 +0800

    baidu

---
+++ baiduImg.js
@@ -0,0 +1,46 @@
+var http=require('http');
+var https=require('https')
+var fs=require('fs');
+var cheerio=require('cheerio');
+
+var wz='http://images.baidu.com/';
+
+
+var strHtml='';
+
+http.get(wz,(res)=>{
+	res.on('data',(chunk)=>{
+		strHtml+=chunk;
+	});
+	
+	res.on('end',()=>{
+		var $=cheerio.load(strHtml);
+		var imgdata=[];
+		$('.img_single_box img').each(function(index,item){
+			imgdata.push($(item).attr('src'))
+			
+		});
+		function saveImage(imgdata){
+			https.get(imgdata,(res)=>{
+				res.setEncoding('binary');
+				var data='';
+				res.on('data',(a)=>{
+					data+=a;
+				}).on('end',()=>{
+					if(!fs.existsSync('./images')){
+						fs.mkdirSync('./images');
+					};
+					fs.writeFile('images/'+Math.random()+'.png',data,'binary',(err)=>{
+						if(!err)
+						console.log('成功')
+					})
+				})
+			});
+		}
+
+		for(var i=0;i<imgdata.length;i++){
+			saveImage(imgdata[i]);
+		}
+		
+	})
+})
